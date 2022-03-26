f981abe557ba06da51f07a796ec39b5b5c6fe509
KeaganQuadras <keaganjackson1997.kj@gmail.com>
2022-03-26 23:48:28 +0530

    removed footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import foot1 from "../Images/timesTsw_logo.webp";
 const Footer = () =>
 {
     return ( <footer>
-                <h3>THIS PROGRAMME IS OFFERED BY </h3>
+                <h3>THIS PROGRAMME IS OFFERED BY</h3>
                     <div className="mt-3 d-flex align-items-center justify-content-center footer_logo">
                         <div className="col-6 col-sm-auto pr-4"><img className="foot-img" src={foot1} alt="" /></div>
                         <div className="col-6 col-sm-auto"><img src={foot2} alt="" /> </div>
